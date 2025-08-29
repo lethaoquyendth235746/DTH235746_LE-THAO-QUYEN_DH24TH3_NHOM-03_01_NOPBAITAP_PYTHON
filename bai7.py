59f941a9965c7a2d7f0e0484f70e358586d36249
lethaoquyendth235746 <quyen_dth235746@student.agu.edu.vn>
2025-08-29 13:27:02 +0700

    thêm file bai7.py

--- bai7.py
+++ bai7.py
@@ -1,3 +1,10 @@
 # viết chương trình in ra một chuỗi ký tự bất kỳ. Nội dung chuỗi được nhập từ tham số dòng lệnh của chương trình.
 import sys
-print('danh sách tham số dòng lệnh:', sys.argv)
+# sys.argv[0] là tên file chương trinh
+# các tham số truyên vào là bắt đầu từ sys.argv[1]
+if len(sys.argv)<=1:
+    print('Vui lòng nhập lệnh qua tham số dòng lệnh')
+else:
+        #ghép các tham số thành một chuỗi hoàn chỉnh
+        chuoi=" ".join(sys.argv[1:])
+        print('chuỗi bạn đã nhập là:',chuoi)
